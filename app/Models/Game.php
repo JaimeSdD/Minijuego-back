5fb4f531031749fe27aeb1c4a021acf7bcbfad1a
JimmyGx6 <jaime92_92@hotmail.com>
2023-03-31 12:51:47 +0200

    modelo de la lógica de juego

---
+++ app/Models/Game.php
@@ -0,0 +1,25 @@
+<?php
+
+class Game
+{
+    public array $players;
+    public array $choices;
+    public array $historical;
+
+    public function randomChoice () {
+
+    }
+
+    public function newGame(){
+
+    }
+
+    public function getHistorical(){
+
+    }
+
+    public function reset(){
+        
+    }
+
+}
