c42f7af6e55ca5c9b66262c66edc9140ac6ce054
JimmyGx6 <jaime92_92@hotmail.com>
2023-04-06 11:56:40 +0200

    Test de la función que computa el resultado

--- tests/Unit/GameTest.php
+++ tests/Unit/GameTest.php
@@ -12,7 +12,27 @@ class GameTest extends TestCase
         $game = new Game();
         $selection = $game->computerSelection();
         $selection1 = $game->computerSelection();
-        $this->assertContains($selection, array_column($game ->choices, 'nombre'), "No existe la selección con ese nombre");
-        $this->assertContains($selection1, array_column($game ->choices, 'nombre'), "No existe la selección con ese nombre");
+        $selection2 = $game->computerSelection();
+
+        $this->assertContains($selection, array_column($game ->choices, "nombre"));
+        $this->assertContains($selection1, array_column($game ->choices, "nombre"));
+        $this->assertContains($selection2, array_column($game ->choices, "nombre"));
+    }
+
+    public function testComputeResult()
+    {
+        $game = new Game();
+
+        $this->assertEquals("victoria", $game->computeResult("piedra", "tijeras"));
+        $this->assertEquals("victoria", $game->computeResult("papel", "piedra"));
+        $this->assertEquals("victoria", $game->computeResult("tijeras", "papel"));
+
+        $this->assertEquals("derrota", $game->computeResult("piedra", "papel"));
+        $this->assertEquals("derrota", $game->computeResult("papel", "tijeras"));
+        $this->assertEquals("derrota", $game->computeResult("tijeras", "piedra"));
+
+        $this->assertEquals("empate", $game->computeResult("piedra", "piedra"));
+        $this->assertEquals("empate", $game->computeResult("papel", "papel"));
+        $this->assertEquals("empate", $game->computeResult("tijeras", "tijeras"));
     }
 }
